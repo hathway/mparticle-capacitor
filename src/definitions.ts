be43b72714718a12bdbc5bc1973e968bea2b5505
Anthony Escobar <10521025+anthonyescobar@users.noreply.github.com>
2021-09-17 14:09:14 -0700

    web hello function

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,3 @@
-declare module "@capacitor/core" {
-  interface PluginRegistry {
-    MParticlePlugin: MParticleCapacitorPlugin;
-  }
-}
-
 export interface MParticleCapacitorPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
   helloMP(): Promise<string>;
