1ef030d9192a2a3854686a2bffe6195db8c9c8b2
Anthony Escobar <10521025+anthonyescobar@users.noreply.github.com>
2021-09-24 12:04:02 -0700

    update 9/24

--- ios/Plugin/MParticleCapacitorPlugin.swift
+++ ios/Plugin/MParticleCapacitorPlugin.swift
@@ -10,30 +10,55 @@ import mParticle_Apple_SDK;
 public class MParticleCapacitorPlugin: CAPPlugin {
     private let implementation = MParticleCapacitor()
 
-    @objc func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        dump("application function ios here")
-       // Override point for customization after application launch.
-        let mParticleOptions = MParticleOptions(key: "us1-b9b520a131dc88448a30babf911b1b51", secret: "_z5GNaX49Nw4c787tBFSmimjZS88Cd_8RVnfxC99cFNbnpFFJxfit_rTL6LA1yP8")
+     @objc func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+         dump("application function ios here")
+        // Override point for customization after application launch.
+         let mParticleOptions = MParticleOptions(key: "us1-279d6248523ab840bb39cfc8d4799691", secret: "wNbwpQ7Rh-W4AHB_Cr2M59YZcFoDiFS8uaOhIB8-MV82Nehtn6zgdbVErbA-ncS7")
+        mParticleOptions.logLevel = MPILogLevel.verbose
+
+        //Please see the Identity page for more information on building this object
+         let request = MPIdentityApiRequest()
+         request.email = "anth92121@wearehathway.com"
         
-       //Please see the Identity page for more information on building this object
-        let request = MPIdentityApiRequest()
-        request.email = "anth92121@wearehathway.com"
-        mParticleOptions.identifyRequest = request
-        mParticleOptions.onIdentifyComplete = { (apiResult, error) in
-            NSLog("Identify complete. userId = %@ error = %@", apiResult?.user.userId.stringValue ?? "Null User ID", error?.localizedDescription ?? "No Error Available")
-        }
+        print("**************************************** REQUEST HERE ***************************")
+        dump(request.email)
+        print("**************************************** REQUEST HERE ***************************")
+
         
-       //Start the SDK
-        MParticle.sharedInstance().start(with: mParticleOptions)
+         mParticleOptions.identifyRequest = request
+         mParticleOptions.onIdentifyComplete = { (apiResult, error) in
+             NSLog("Identify complete. userId = %@ error = %@", apiResult?.user.userId.stringValue ?? "Null User ID", error?.localizedDescription ?? "No Error Available")
+         }
         
-       return true
-    }
+        //Start the SDK
+         MParticle.sharedInstance().start(with: mParticleOptions)
+        
+        return true
+     }
 
     @objc func mParticleInit(_ call: CAPPluginCall) {
         self.notifyListeners("mParticleInit", data: ["value":"I am init'd"])
         let value = call.getString("value") ?? ""
         dump("i am ios mparticleinit call:")
         dump(value)
+
+        let mParticleOptions = MParticleOptions(key: "us1-279d6248523ab840bb39cfc8d4799691", secret: "wNbwpQ7Rh-W4AHB_Cr2M59YZcFoDiFS8uaOhIB8-MV82Nehtn6zgdbVErbA-ncS7")
+        
+        mParticleOptions.logLevel = MPILogLevel.verbose
+       //Please see the Identity page for more information on building this object
+        let request = MPIdentityApiRequest()
+        request.email = "anth92121@wearehathway.com"
+        print("**************************************** REQUEST HERE ***************************")
+        dump(request.email)
+        print("**************************************** REQUEST HERE ***************************")
+        mParticleOptions.identifyRequest = request
+        mParticleOptions.onIdentifyComplete = { (apiResult, error) in
+            NSLog("Identify complete. userId = %@ error = %@", apiResult?.user.userId.stringValue ?? "Null User ID", error?.localizedDescription ?? "No Error Available")
+        }
+        dump(mParticleOptions)
+       //Start the SDK
+        MParticle.sharedInstance().start(with: mParticleOptions)
+
         call.resolve([
             "value": implementation.echo(value)
         ])
